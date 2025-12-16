9732df84ba4272d57f4300028cc71ed68bd83284
Open Server <webmaster@openserver.com.pe>
2025-12-15 23:52:05 -0500

    chore: Initial commit - phrases API base with webflux

---
+++ src/main/java/pe/com/openserver/phrases/PhrasesServiceApplication.java
@@ -0,0 +1,13 @@
+package pe.com.openserver.phrases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhrasesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhrasesServiceApplication.class, args);
+	}
+
+}
